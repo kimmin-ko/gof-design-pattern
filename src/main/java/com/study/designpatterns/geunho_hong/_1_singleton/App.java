6853c01983fb4cc07e2e1ac39f8ab870c91308c7
GuenhoHong <hogu8159@naver.com>
2022-04-10 14:35:58 +0900

    feat: Singleton and Factory method pattern

--- src/main/java/com/study/designpatterns/geunho_hong/_1_singleton/App.java
+++ src/main/java/com/study/designpatterns/geunho_hong/_1_singleton/App.java
@@ -1,4 +1,19 @@
 package com.study.designpatterns.geunho_hong._1_singleton;
 
 public class App {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Settings5 settings = Settings5.INSTANCE;
+
+        Settings5 settings1 = null;
+        try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))) {
+            out.writeObject(settings);
+        }
+
+        try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))) {
+            settings1 = (Settings5) in.readObject();
+        }
+
+        System.out.println(settings == settings1);
+    }
 }
