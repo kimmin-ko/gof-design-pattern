f5835bb8fe44187ba2e8eec50ec8fe7450e19a2b
kimmin <kimmin2301@gmail.com>
2022-04-21 02:38:56 +0900

    빌더 패턴 완료

---
+++ src/main/java/com/study/designpatterns/min_kim/_4_builder/_02_after/TourPlanBuilder.java
@@ -0,0 +1,21 @@
+package com.study.designpatterns.min_kim._4_builder._02_after;
+
+import com.study.designpatterns.min_kim._4_builder._01_before.TourPlan;
+
+import java.time.LocalDate;
+
+public interface TourPlanBuilder {
+
+    TourPlanBuilder title(String title);
+
+    TourPlanBuilder nightsAndDays(int nights, int days);
+
+    TourPlanBuilder startDate(LocalDate startDate);
+
+    TourPlanBuilder whereToStay(String whereToStay);
+
+    TourPlanBuilder addPlan(int day, String plan);
+
+    TourPlan getPlan();
+
+}
