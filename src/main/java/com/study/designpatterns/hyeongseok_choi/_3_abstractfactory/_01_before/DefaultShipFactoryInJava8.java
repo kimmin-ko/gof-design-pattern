5e51b36c702d02226384a8a70adf1754a13d6ecd
Einhugo <aeinhugo@gmail.com>
2022-04-24 09:32:46 +0900

    feat: add abstract factory

---
+++ src/main/java/com/study/designpatterns/hyeongseok_choi/_3_abstractfactory/_01_before/DefaultShipFactoryInJava8.java
@@ -0,0 +1,25 @@
+package com.study.designpatterns.hyeongseok_choi._3_abstractfactory._01_before;
+
+
+public abstract class DefaultShipFactoryInJava8 implements ShipFactoryInJava8 {
+
+    @Override
+    public void sendEmailTo(String email, Ship ship) {
+        System.out.println(ship.getName() + " 다 만들었습니다.");
+    }
+
+    @Override
+    public void validate(String name, String email) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("배 이름을 지어주세요.");
+        }
+        if (email == null || email.isBlank()) {
+            throw new IllegalArgumentException("연락처를 남겨주세요.");
+        }
+    }
+
+    @Override
+    public void prepareFor(String name) {
+        System.out.println(name + " 만들 준비 중");
+    }
+}
---
+++ src/main/java/com/study/designpatterns/hyeongseok_choi/_3_abstractfactory/_02_after/WhiteShipFactory.java
@@ -0,0 +1,19 @@
+package com.study.designpatterns.hyeongseok_choi._3_abstractfactory._02_after;
+
+
+public class WhiteShipFactory extends DefaultShipFactoryInJava8 {
+
+    private ShipPartsFactory shipPartsFactory;
+
+    public WhiteShipFactory(ShipPartsFactory shipPartsFactory) {
+        this.shipPartsFactory = shipPartsFactory;
+    }
+
+    @Override
+    public Ship createShip() {
+        Ship ship = new Ship();
+        ship.setAnchor(shipPartsFactory.createAnchor());
+        ship.setWheel(shipPartsFactory.createWheel());
+        return ship;
+    }
+}
