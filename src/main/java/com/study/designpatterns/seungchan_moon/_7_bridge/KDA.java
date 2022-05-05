52e87ac5f945d6e048e4a5a1c0adc9558388df9b
noah <m3252@naver.com>
2022-05-05 19:58:18 +0900

    feat : 브릿지 패턴

---
+++ src/main/java/com/study/designpatterns/seungchan_moon/_7_bridge/KDA.java
@@ -0,0 +1,8 @@
+package com.study.designpatterns.seungchan_moon._7_bridge;
+
+public class KDA implements Skin{
+    @Override
+    public String getName() {
+        return "KDA";
+    }
+}
---
+++ src/main/java/com/study/designpatterns/seungchan_moon/_7_bridge/PoolParty.java
@@ -0,0 +1,8 @@
+package com.study.designpatterns.seungchan_moon._7_bridge;
+
+public class PoolParty implements Skin {
+    @Override
+    public String getName() {
+        return "PoolParty";
+    }
+}
