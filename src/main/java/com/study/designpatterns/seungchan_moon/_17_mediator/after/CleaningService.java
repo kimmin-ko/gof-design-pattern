9c8e71ef4e1b5e091762e8f83f9189fb54c6a175
noah <m3252@naver.com>
2022-06-07 23:44:49 +0900

    feat : 중재자 패턴 추가

---
+++ src/main/java/com/study/designpatterns/seungchan_moon/_17_mediator/after/CleaningService.java
@@ -0,0 +1,10 @@
+package com.study.designpatterns.seungchan_moon._17_mediator.after;
+
+public class CleaningService {
+    private FrontDesk frontDesk = new FrontDesk();
+
+    public void getTowers(Integer guestId, int numberOfTowers) {
+        String roomNumber = this.frontDesk.getRoomNumberFor(guestId);
+        System.out.println("provide " + numberOfTowers + " to " + roomNumber);
+    }
+}
