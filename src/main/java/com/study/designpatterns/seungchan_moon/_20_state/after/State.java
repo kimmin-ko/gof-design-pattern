3ed433e48b0eda5f642e8db5abd606fe34b2a067
noah <m3252@naver.com>
2022-06-26 19:54:52 +0900

    feat : 상태 패턴

---
+++ src/main/java/com/study/designpatterns/seungchan_moon/_20_state/after/State.java
@@ -0,0 +1,10 @@
+package com.study.designpatterns.seungchan_moon._20_state.after;
+
+public interface State {
+
+    void addReview(String review, Student student);
+    void addStudent(Student student);
+
+}
+
+
