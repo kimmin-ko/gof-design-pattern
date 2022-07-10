dd34f5b713976d4766694838bc46d78af91656a0
hong8159 <hogu8159@naver.com>
2022-07-10 22:21:21 +0900

    feature: iteratior,mediator pattern add

---
+++ src/main/java/com/study/designpatterns/geunho_hong/_17_mediator/_01_before/Restaurant.java
@@ -0,0 +1,13 @@
+package com.study.designpatterns.geunho_hong._17_mediator._01_before;
+
+public class Restaurant {
+
+    private CleaningService cleaningService = new CleaningService();
+    public void dinner(Guest guest) {
+        System.out.println("dinner " + guest);
+    }
+
+    public void clean() {
+        cleaningService.clean(this);
+    }
+}
---
+++ src/main/java/com/study/designpatterns/geunho_hong/_17_mediator/_02_after/CleaningService.java
@@ -0,0 +1,11 @@
+package com.study.designpatterns.geunho_hong._17_mediator._02_after;
+
+public class CleaningService {
+
+    private FrontDesk frontDesk = new FrontDesk();
+
+    public void getTowers(Integer guestId, int numberOfTowers) {
+        String roomNumber = this.frontDesk.getRoomNumberFor(guestId);
+        System.out.println("provide " + numberOfTowers + " to " + roomNumber);
+    }
+}
